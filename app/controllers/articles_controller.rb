a71bf69ea88a260b11de5872d393d3c613d24a0c
grace-mugoiri <gracemugoiri@gmail.com>
2019-06-19 17:05:31 +0300

    added create method and helper files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        # fail #halts the request allowing you to examine the request parameters
+        @article = Article.new(params[:article])
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
